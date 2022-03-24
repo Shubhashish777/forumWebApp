52ebc704ca2f02c0612b177d63809290f47c0f3a
Shubhashish777 <shubhashish.saha@outlook.com>
2022-03-24 05:42:33 +0530

    Init Project

---
+++ src/test/java/com/app/forumWebApp/ForumWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.forumWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
